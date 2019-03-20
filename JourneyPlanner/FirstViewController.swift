108fe771173a3f3791a4f5b33b8ceaf87194f811
RuotongX <bagadiyi@gmail.com>
2019-03-20 19:58:16 +1300

    Initial Commit

---
+++ JourneyPlanner/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  JourneyPlanner
+//
+//  Created by RuotongX on 2019/3/20.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ JourneyPlanner/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  JourneyPlanner
+//
+//  Created by RuotongX on 2019/3/20.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
