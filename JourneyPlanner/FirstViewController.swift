a60d24b8d6f1d528587cfd03c374c48dd15b4603
daltonchen <31174316+daltonchen@users.noreply.github.com>
2019-04-09 09:00:28 +1200

    add tab to the bottom

--- JourneyPlanner/FirstViewController.swift
+++ JourneyPlanner/FirstViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
+        // used to load the view, after loading, it can customize items - Dalton Chen
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
