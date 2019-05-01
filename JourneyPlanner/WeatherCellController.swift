c79593d6a80527c1861b291e770ff91773c9c2ae
RuotongX <bagadiyi@gmail.com>
2019-05-01 23:39:57 +1200

    Add the Weather forcast screen.

---
+++ JourneyPlanner/WeatherCellController.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherCellController.swift
+//  JourneyPlanner
+//
+//  Created by RuotongX on 2019/5/1.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCellController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ JourneyPlanner/WeatherForcastController.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherForcastController.swift
+//  JourneyPlanner
+//
+//  Created by RuotongX on 2019/5/1.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class WeatherForcastController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+   
+
+}
