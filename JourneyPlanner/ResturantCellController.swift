40ce087b0fe18fbc3b682798145b1bed92bb2590
RuotongX <bagadiyi@gmail.com>
2019-05-05 15:19:53 +1200

    Add the restaurant function

---
+++ JourneyPlanner/ResturantCellController.swift
@@ -0,0 +1,50 @@
+//
+//  ResturantCellController.swift
+//  JourneyPlanner
+//
+//  Created by RuotongX on 2019/5/4.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class ResturantCellController: UITableViewCell {
+
+    
+    @IBOutlet weak var ResturantImage: UIImageView!
+    @IBOutlet weak var ResturantName: UILabel!
+    @IBOutlet weak var ResturantMark: UILabel!
+    @IBOutlet weak var AvangeCost: UILabel!
+    @IBOutlet weak var ResturantType: UILabel!
+    @IBOutlet weak var Rank: UILabel!
+    
+    var Url :String = ""
+    var lat : Double = 0.0
+    var lon : Double = 0.0
+    
+    func setResturant(resturant:Resturant){
+        ResturantImage.image = resturant.RImage
+        ResturantName.text = resturant.RName
+        ResturantMark.text = resturant.RMark
+        AvangeCost.text = "$ "+"\(resturant.RCost)"
+        ResturantType.text = resturant.RType
+        self.Url = resturant.RUrl
+        self.lat = resturant.Rlat
+        self.lon = resturant.Rlon
+        self.Rank.text = "\(resturant.Rank)"
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+//    @IBAction func ResturantLocation(_ sender: Any) {
+//    }
+}
---
+++ JourneyPlanner/ResturantRecommandedController.swift
@@ -0,0 +1,112 @@
+//
+//  ResturantRecommandedController.swift
+//  JourneyPlanner
+//
+//  Created by RuotongX on 2019/5/4.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import Foundation
+import SwiftyJSON
+
+class ResturantRecommandedController: UIViewController {
+    
+    @IBOutlet weak var Table: UITableView!
+    let lat = UserDefaults().string(forKey: "lat")
+    let lon = UserDefaults().string(forKey: "lon")
+    
+    var resturants: [Resturant] = [];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getResturants()
+    }
+    
+    @IBAction func Return(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func getResturants(){
+        let header = "1143149f226cce509acd087c44290754"
+        
+        Alamofire.request("https://developers.zomato.com/api/v2.1/geocode?apikey=\(header)&lat=\(lat!)&lon=\(lon!)").responseJSON{
+            response in
+            if let responseStr = response.result.value{
+                let jsonResponse = JSON(responseStr)
+                for i in 0...jsonResponse["nearby_restaurants"].array!.count-1{
+                    let jsonRest = jsonResponse["nearby_restaurants"].array![i]
+                    let jsonR = jsonRest["restaurant"]
+                    let jsonRating = jsonR["user_rating"]
+                    let jsonLocation = jsonR["location"]
+                    let Name = jsonR["name"].stringValue
+                    let Url = jsonR["url"].stringValue
+                    let Price = jsonR["average_cost_for_two"].doubleValue/2
+                    let rate = jsonRating["aggregate_rating"].stringValue
+                    let cuisines = jsonR["cuisines"].stringValue
+                    let lat = jsonLocation["latitude"].doubleValue
+                    let lon = jsonLocation["longitude"].doubleValue
+                    let image = jsonR["thumb"].stringValue
+                    let resturant = Resturant()
+                    let url = URL(string: image)
+                    let votes = jsonRating["votes"].intValue
+                    if let url = url{
+                        do {
+                            let data = try Data(contentsOf: url)
+                            resturant.RImage = UIImage(data: data)!
+                        }catch let error as NSError {
+                            print(error)
+                        }
+                    }
+                    resturant.RName = Name
+                    resturant.RCost = Price
+                    resturant.RMark = rate
+                    resturant.RType = cuisines
+                    resturant.Rlat = lat
+                    resturant.Rlon = lon
+                    resturant.RUrl = Url
+                    resturant.votes = votes
+                    self.resturants.append(resturant)
+                }
+                DispatchQueue.main.async {
+                    for i in 0...self.resturants.count-1{
+                        for j in 1...self.resturants.count-2{
+                            if self.resturants[i].votes<self.resturants[j].votes{
+                                let temp = self.resturants[i]
+                                self.resturants[i] = self.resturants[j]
+                                self.resturants[j] = temp
+                            }
+                        }
+                    }
+                    for k in 0...self.resturants.count-1{
+                        self.resturants[k].Rank = k+1
+                    }
+                    self.Table.reloadData()
+                }
+            }
+        }
+
+    }
+}
+
+extension  ResturantRecommandedController: UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return resturants.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let resturant = resturants[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "resturantcell", for: indexPath) as! ResturantCellController
+        cell.setResturant(resturant: resturant)
+        return cell
+    }
+    
+    
+}
