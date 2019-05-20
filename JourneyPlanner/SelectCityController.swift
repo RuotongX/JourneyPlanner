d1151310d5f691068d0a59dc5a4aa7b20a1a3c80
Basco12138 <tnb5436@autuni.ac.nz>
2019-05-20 20:28:54 +1200

    Build the frame of select city page

---
+++ JourneyPlanner/SelectCityController.swift
@@ -0,0 +1,17 @@
+//
+//  SelectCityController.swift
+//  JourneyPlanner
+//
+//  Created by ZHE WANG on 20/05/19.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import UIKit
+
+class SelectCityController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ JourneyPlanner/SelectCityTableviewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SelectCityTableviewCell.swift
+//  JourneyPlanner
+//
+//  Created by ZHE WANG on 20/05/19.
+//  Copyright © 2019 RuotongX. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SelectCityTableviewCell : UITableViewCell{
+    
+    @IBOutlet weak var CityImage: UIImageView!
+    
+    @IBOutlet weak var CityName: UILabel!
+    
+    
+}
