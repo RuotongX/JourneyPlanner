bfe740e9a1fddddecc6c5fbc2a48c9e992cfec9e
daltonchen <31174316+daltonchen@users.noreply.github.com>
2019-05-30 18:38:11 +1200

    add more attraction information, fix minor bug

--- JourneyPlanner/RouteListViewController.swift
+++ JourneyPlanner/RouteListViewController.swift
@@ -102,12 +102,11 @@ class RouteListViewController: UIViewController {
             if let selectCityController = segue.destination as? RouteSelectCityController{
                 selectCityController.delgate = self
                 
-//                if let cell = sender as? UITableViewCell{
-//                    if let indexPath = TripTableView.indexPath(for: cell),
-//                        let city = routeInfo[indexPath.row].Cities{
-//                            selectCityController.cityInformation = city
-//                    }
-//                }
+                if let cell = sender as? UITableViewCell{
+                    if let indexPath = TripTableView.indexPath(for: cell){
+                        selectCityController.routeName = routeInfo[indexPath.row].routeName
+                    }
+                }
             }
         }
     }
